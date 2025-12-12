505a61ef69e8a9a0965cdc7782a171b7e1c40216
JSkahl <josekahl333@gmail.com>
2025-12-12 10:10:55 -0300

    chore: clean up project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+    console.log("Página em construção");
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="w-full h-full flex flex-col justify-center items-center">
+        <h1 class="text-4xl font-bold mb-4">Página em construção</h1>
+        <p>Só um segundo!</p>
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
