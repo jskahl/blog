e7749fad59dccc4bc7d881459e30b2d10af0c23e
JSkahl <josekahl333@gmail.com>
2025-12-13 19:40:24 -0300

    feat: create basic global layout

---
+++ src/layout/Layout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'; 
+</script>
+
+<template>
+    <div class="w-full h-full flex flex-col items-center">
+        <HeaderComponent />
+        <div class="">
+            <router-view class="flex-1 overflow-auto" />
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    console.log("Página em construção");
+</script>
+
+<template>
+    <div class="w-full h-full flex flex-col justify-center items-center">
+        <h1 class="text-4xl font-bold mb-4">Blog em construção</h1>
+        <p>Só um segundo!</p>
+    </div>
+</template>
