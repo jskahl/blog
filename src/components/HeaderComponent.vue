d52b288f05c7382bbe558524ba087ea3decef468
JSkahl <josekahl333@gmail.com>
2025-12-13 19:40:34 -0300

    feat: create initial header structure

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div class="
+        w-full md:w-1/2 h-16
+        flex justify-between items-center
+        px-6 mb-8 mx-2
+        border-b-2 border-b-white
+        ">
+        <router-link to="/">
+            <h1>Blog</h1> 
+        </router-link>
+
+        <div class="">
+        </div>
+    </div>
+</template>
