d1f38d8c65720b9c8c8e261fdad6238de9d482b5
JSkahl <josekahl333@gmail.com>
2025-12-14 12:18:07 -0300

    feat: add color scheme in ap.vue

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,11 @@ import { RouterView } from "vue-router";
 <template>
     <RouterView />
 </template>
+
+<style scoped>
+@import "tailwindcss";
+
+body {
+    @apply bg-(var[--text-color]) text-(var[--bg-color]) dark:bg-(var[--bg-color]) dark:text-(var[--text-color]);
+}
+</style>
