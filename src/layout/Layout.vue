84846bfeb38519aec30ed92e43629c77df0aa5dd
JSkahl <josekahl333@gmail.com>
2025-12-14 12:17:26 -0300

    feat: implement new layout

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import HeaderComponent from '@/components/HeaderComponent.vue'; 
+import FooterComponent from '@/components/FooterComponent.vue';
 </script>
 
 <template>
-    <div class="w-full h-full flex flex-col items-center">
-        <HeaderComponent />
-        <div class="">
-            <router-view class="flex-1 overflow-auto" />
-        </div>
+  <div class="min-h-screen flex flex-col items-center px-2 md:px-0">
+    <HeaderComponent />
+
+    <div class="w-full md:w-1/2 flex flex-col items-center flex-1">
+      <router-view />
+      <FooterComponent />
     </div>
+  </div>
 </template>
