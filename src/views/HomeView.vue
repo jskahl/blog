0502d07465241432cb9d72a6671c14df9c38d0cb
JSkahl <josekahl333@gmail.com>
2025-12-14 12:17:55 -0300

    fix: fix css issue in homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,9 @@
 </script>
 
 <template>
-    <div class="w-full h-full flex flex-col justify-center items-center">
+    <div class="w-full h-full flex flex-col items-center">
         <h1 class="text-4xl font-bold mb-4">Blog em construção</h1>
         <p>Só um segundo!</p>
+
     </div>
 </template>
