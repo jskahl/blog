92777701ede245160516720dd39b21b0e49d187f
JSkahl <josekahl333@gmail.com>
2025-12-14 12:17:12 -0300

    chore: finish basic header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,27 +1,40 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { ThemeLightDark, WhiteBalanceSunny, MoonWaningCrescent } from '@/icons';
+import { ref, watch } from "vue";
+import { WeatherSunny, WeatherNight } from "@/icons";
+import { useDark, useToggle } from "@vueuse/core";
 
-const theme = ref("dark")
-const isHover = ref(false);
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+
 </script>
 
 <template>
-    <div class="
-        w-full md:w-1/2 h-16
-        flex justify-between items-center
-        px-6 mb-8 mx-2
-        border-b-2 border-b-white
-        ">
-        <router-link to="/">
-            <h1>Blog</h1> 
-        </router-link>
-        <div>
-            <div class="cursor-pointer hover:text-green-400" @mouseover="isHover = true" @mouseleave="isHover = false" @click="theme = theme === 'light' ? 'dark' : 'light'">
-                <ThemeLightDark v-if="isHover" />
-                <WhiteBalanceSunny v-if="theme === 'light' && !isHover" />
-                <MoonWaningCrescent v-if="theme ==='dark' && !isHover" />
-            </div>
-        </div>
+  <header
+    class="w-full md:w-1/2 h-16
+    bg-(--text-color) dark:bg-(--bg-color)
+    sticky top-0 z-50
+    flex justify-between items-center
+    px-3 md:px-6 mb-8 mx-2
+    border-b-2 border-b-(--text-color)"
+  >
+    <router-link to="/">
+      <h1>Blog</h1>
+    </router-link>
+    <div>
+      <div
+        class="
+            cursor-pointer 
+            hover:text-green-400 active:text-green-400
+            hover:rotate-12 active:scale-95 transition-transform
+            "
+        @click="toggleDark()"
+        title="Tema"
+      >
+          <WeatherSunny v-if="!isDark" />
+          <WeatherNight v-if="isDark" />
+      </div>
     </div>
+
+    <div class="w-10 h-10 bg-green-200 dark:bg-red-900">aaa</div>
+  </header>
 </template>
