68c548a4af316d1c88db514fceb5df95d4e53d91
JSkahl <josekahl333@gmail.com>
2025-12-16 20:51:16 -0300

    toc

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -6,6 +6,7 @@ import MarkdownItTaskLists from "markdown-it-task-lists";
 import MarkdownItHighlightjs from "markdown-it-highlightjs";
 import MarkdownItAnchor from "markdown-it-anchor";
 import MarkdownItCodeCopy from "markdown-it-code-copy";
+import MarkdownTOCDoneRight from "markdown-it-toc-done-right";
 import frontMatter from "markdown-it-front-matter";
 import yaml from "js-yaml";
 
@@ -33,9 +34,23 @@ md.use(MarkdownItAnchor, {
         class: "text-green-800 dark:text-green-400",
     }),
 });
+md.use(MarkdownTOCDoneRight);//, {
+//    level: [1, 2, 3],
+//    containerClass: "toc",
+//    listType: "ul",
+//    callback: (html: string) => {
+//        return `
+//      <details class="toc-dropdown bg-red-200">
+//        <summary class="toc-summary bg-red-500">Contents</summary>
+//        ${html}
+//      </details>
+//    `;
+//    },
+//});
 
 md.use(MarkdownItCodeCopy, {
-    buttonClass: "text-inherit [&_span]:text-(--text-color) dark:[&_span]:text-(--bg-color) text-red-200 hover:scale-110 transition-transform active:scale-90 duration-100",
+    buttonClass:
+        "text-inherit [&_span]:text-(--text-color) dark:[&_span]:text-(--bg-color) text-red-200 hover:scale-110 transition-transform active:scale-90 duration-100",
 });
 
 const modules = import.meta.glob("../posts/*.md", {
@@ -60,7 +75,7 @@ onMounted(async () => {
 
 <template>
     <article
-        class="prose dark:prose-invert prose-a:decoration-wavy prose-a:decoration-green-800 dark:prose-a:decoration-green-400 prose-headings:scroll-mt-20 prose-h1:text-green-800 dark:prose-h1:text-green-400 prose-p:text-justify markdown prose-blockquote:prose-p:first-of-type:after:content-none prose-blockquote:prose-p:first-of-type:before:content-none prose-code:before:content-none prose-code:after:content-none not-prose-pre:prose-code:bg-(--bg-color) dark:not-prose-pre:prose-code:bg-(--text-color) not-prose-pre:prose-code:text-(--text-color) dark:not-prose-pre:prose-code:text-(--bg-color) not-prose-pre:prose-code:rounded-sm not-prose-pre:prose-code:p-1 text-inherit bg-inherit prose-blockquote:prose-p:opacity-50 prose-strong:font-extrabold prose-strong:text-green-800 dark:prose-strong:font-extrabold dark:prose-strong:text-green-400 prose-p prose-pre:bg-(--bg-color) prose-pre:bg(--text-color) dark:prose-pre:bg-(--text-color) dark:prose-pre:text-(--bg-color) dark:prose-pre:bg-(--text-color) dark:prose-pre:text-(--bg-color) prose-blockquote:quotes prose-blockquote:border-l-green-800 dark:prose-blockquote:border-l-green-400 prose-li:marker:text-green-800 dark:prose-li:marker:text-green-400 w-full h-full">
-        <div v-html="html" />
+        class="prose dark:prose-invert prose-a:decoration-wavy prose-a:decoration-green-800 dark:prose-a:decoration-green-400 prose-headings:scroll-mt-20 prose-h1:text-green-800 dark:prose-h1:text-green-400 prose-p:text-justify markdown prose-blockquote:prose-p:first-of-type:after:content-none prose-blockquote:prose-p:first-of-type:before:content-none prose-code:before:content-none prose-code:after:content-none not-prose-pre:prose-code:bg-(--bg-color) dark:not-prose-pre:prose-code:bg-(--text-color) not-prose-pre:prose-code:text-(--text-color) dark:not-prose-pre:prose-code:text-(--bg-color) not-prose-pre:prose-code:rounded-sm not-prose-pre:prose-code:p-1 text-inherit bg-amber-400 prose-blockquote:prose-p:opacity-50 prose-strong:font-extrabold prose-strong:text-green-800 dark:prose-strong:font-extrabold dark:prose-strong:text-green-400 prose-p prose-pre:bg-(--bg-color) prose-pre:bg(--text-color) dark:prose-pre:bg-(--text-color) dark:prose-pre:text-(--bg-color) dark:prose-pre:bg-(--text-color) dark:prose-pre:text-(--bg-color) prose-blockquote:quotes prose-blockquote:border-l-green-800 dark:prose-blockquote:border-l-green-400 prose-li:marker:text-green-800 dark:prose-li:marker:text-green-400 w-full h-full">
+        <div v-html="html" class="w-full h-full bg-green-500"/>
     </article>
 </template>
