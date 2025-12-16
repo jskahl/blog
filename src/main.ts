e19a47eef64d4404c35817960ba49dd724ab10a0
JSkahl <josekahl333@gmail.com>
2025-12-16 11:08:02 -0300

    build: add packages for theme and markdown renderer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import "./assets/main.css";
 import App from "./App.vue";
 import router from "./router/index.ts";
+import 'highlight.js/styles/github.css'
+import 'highlight.js/styles/github-dark.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from "vue";
 
